2cbafa9974caa05ee5e14fdec00104f672e15bc6
Nick Jensen <nick@botsparks.com>
2019-08-30 09:48:31 -0400

    Intiial

---
+++ js/bot.js
@@ -0,0 +1,43 @@
+var botui = new BotUI('my-botui-app');
+
+botui.message.add({
+        delay: 500,
+        loading: true,
+        content: 'Hi, I\'m Nick! 👋 🙂️'
+      }).then(function () { // wait till previous message has been shown.
+
+        botui.message.add({
+          delay: 2000,
+          loading: true,
+          content: 'I help business owners create engaging digital experiences that drive revenue. Just text me what you’re primary marketing objective is on FB Messenger'
+        }).then(function(){
+          botui.message.add({
+            delay:1000,
+            loading:true,
+            content: 'I’ll send you the best options to help you increase sales'
+          })
+          .then(function(){
+            botui.message.add({
+              delay:1000,
+              loading:true,
+              content: 'Tell me which you idea love and I’ll send you more details 👍'
+            })
+            .then(function(){
+              botui.message.add({
+                delay:1000,
+                loading:true,
+                content: 'Think of me as your secret marketing super power 💪🤵'
+              })
+              .then(function(){
+                botui.message.add({
+                  delay:500,
+                //  loading:true,
+                  cssClass: 'only-btn',
+                  content: '<a href="https://m.me/connectwnick" class="btn btn-primary"><!----> Click here to Start</a>'
+                })
+
+              });
+            });
+          });
+        });
+      });
